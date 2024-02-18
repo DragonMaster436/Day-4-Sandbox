a95a772dc106fe6403e000b920c3a45ab28183ac
DragonMaster436 <96381483+DragonMaster436@users.noreply.github.com>
2024-02-17 20:23:49 -0600

    Add files via upload

---
+++ Day-4-Sandbox.py
@@ -0,0 +1,42 @@
+# we are looking at ways to randomize coding! 
+# Kahn Academy has a great pseudorandom randomize generator
+# https://www.khanacademy.org/computing/computer-science/cryptography/crypt/v/random-vs-pseudorandom-number-generators
+
+# import random
+
+# random_integer = random.randint(1, 10)
+
+# print(random_integer)
+
+# this code imports a module for a randomize function 
+# my best guess the random in the random_integer is taging the inport and the randint 
+# after that is telling it to pick a number between those two numbers
+
+# import sandbox_module
+
+# print(sandbox_module.birthday)
+
+# random_float = random.random() * 5
+# print(random_float)
+
+# the * 5 allows the user to select a random number from 0 to 5
+
+# [] is always related to lists 0 starts at the top of the list and -1 starts at the bottom of the list
+
+
+
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# fruits[-1] = "Melons"
+# fruits.append("Lemons")
+# print(fruits)
+# fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+# vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+# dirty_dozen = [fruits, vegetables]
+ 
+# print(dirty_dozen[0][3])
+test = input("Give me numebr")
+num1 = int(test[0])
+num2 = int(test[1])
+print(num1)
+print(num2)
